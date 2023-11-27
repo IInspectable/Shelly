ecf1531155d012cefa749ee3587f6153ac64383d
IInspectable <github@smjh.de>
2023-11-27 09:50:07 +0100

    fmt

--- Shelly25.cs
+++ Shelly25.cs
@@ -12,7 +12,7 @@ public class Shelly25 {
     }
 
     public required HttpClient HttpClient { get; init; }
-    public required string BaseUri { get; init; }
+    public required string     BaseUri    { get; init; }
 
     public Channel Channel0 => Channels[0];
     public Channel Channel1 => Channels[1];
@@ -22,20 +22,22 @@ public class Shelly25 {
     public class Channel {
 
         public required Shelly25 Shelly { get; init; }
-        public required int Index { get; init; }
+        public required int      Index  { get; init; }
 
-        public string RelayUri => $"{Shelly.BaseUri}/relay/{Index}";
-        public string MeterUri => $"{Shelly.BaseUri}/meter/{Index}";
+        public string RelayUri         => $"{Shelly.BaseUri}/relay/{Index}";
+        public string MeterUri         => $"{Shelly.BaseUri}/meter/{Index}";
         public string RelaySettingsUri => $"{Shelly.BaseUri}/settings/relay/{Index}";
 
         public async Task<RelayData?> GetRelayAsync() => await GetAsync<RelayData>(Shelly.HttpClient, RelayUri);
         public async Task<MeterData?> GetMeterAsync() => await GetAsync<MeterData>(Shelly.HttpClient, MeterUri);
         public async Task<RelaySettingsData?> GetRelaySettingsAsync() => await GetAsync<RelaySettingsData>(Shelly.HttpClient, RelaySettingsUri);
+
     }
 
     static async Task<T?> GetAsync<T>(HttpClient client, string requestUri) {
         var response = await client.GetAsync(requestUri);
-        var json = await response.Content.ReadFromJsonAsync<T>();
+        var json     = await response.Content.ReadFromJsonAsync<T>();
         return json;
     }
+
 }
